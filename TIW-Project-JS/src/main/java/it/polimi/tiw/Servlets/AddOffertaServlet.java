efef5470fb73072a31843eddeef4f3372263a218
pietro <pietrogentile03@gmail.com>
2025-05-10 10:25:20 +0200

    impostazione base della pagina principale html

--- TIW-Project-JS/src/main/java/it/polimi/tiw/Servlets/AddOffertaServlet.java
+++ TIW-Project-JS/src/main/java/it/polimi/tiw/Servlets/AddOffertaServlet.java
@@ -1,32 +1,36 @@
 package it.polimi.tiw.Servlets;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 public class AddOffertaServlet extends HttpServlet {
-  private final Gson gson = new Gson();
+	private static final long serialVersionUID = 1L;
+	private final Gson gson = new Gson();
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException {
-    response.setContentType("application/json;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    JsonObject jo = new JsonObject();
-    try {
-      NewOffertaDto dto = gson.fromJson(request.getReader(), NewOffertaDto.class);
-      OffertaService.submitOffer(dto);
-      jo.addProperty("success", true);
-      out.print(gson.toJson(jo));
-    } catch (IllegalArgumentException ia) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      jo.addProperty("error", ia.getMessage());
-      out.print(gson.toJson(jo));
-    } catch (Exception e) {
-      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      jo.addProperty("error", "Errore inserimento offerta: " + e.getMessage());
-      out.print(gson.toJson(jo));
-    }
-    out.flush();
+//    response.setContentType("application/json;charset=UTF-8");
+//    PrintWriter out = response.getWriter();
+//    JsonObject jo = new JsonObject();
+//    try {
+//      NewOffertaDto dto = gson.fromJson(request.getReader(), NewOffertaDto.class);
+//      OffertaService.submitOffer(dto);
+//      jo.addProperty("success", true);
+//      out.print(gson.toJson(jo));
+//    } catch (IllegalArgumentException ia) {
+//      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+//      jo.addProperty("error", ia.getMessage());
+//      out.print(gson.toJson(jo));
+//    } catch (Exception e) {
+//      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//      jo.addProperty("error", "Errore inserimento offerta: " + e.getMessage());
+//      out.print(gson.toJson(jo));
+//    }
+//    out.flush();
   }
 }
