42cd28710d8b91d077cbd226ce54124c37b04776
StefanoGarzoni <stefano.garzoni@mail.polimi.it>
2025-05-13 12:28:36 +0200

    update post meeting Stefano

--- TIW-Project-JS/src/main/webapp/js/main.js
+++ TIW-Project-JS/src/main/webapp/js/main.js
@@ -22,22 +22,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-
+//NOTA DOVREBBE ESSERE RIMOSSO E GESTITO TUTTO NELLE SERVLET
 //--------------------------------------------------------------------------------------------
 //CREAZIONE INIZIALE DEI COOKIE
 
 // Dichiaro i tre nomi dei cookie
-var cookieNames = ['renderTableAsteAperte', 'renderAllTablesAste'];
+var cookieNames = ['renderTableAsteAperte', 'renderAllTablesAste', 'renderTableAsteVisionate'];
 
 //scadenza di una settimana
 var oneWeek = 7 * 24 * 60 * 60;
 
 //scorro tutti i cookie
 for (var i = 0; i < cookieNames.length; i++) {
-    document.cookie = 
+    if(cookieNames[i] === 'renderTableAsteVisionate') {
+        // Se il cookie è renderTableAsteVisionate, lo imposto a false
+        document.cookie = 
+            cookieNames[i] + '=' + encodeURIComponent("False") +
+            '; max-age=' + oneWeek +
+            '; path=/';
+    }else{
+        document.cookie = 
         cookieNames[i] + '=' + encodeURIComponent("True") +
         '; max-age=' + oneWeek +
         '; path=/';
+    }
+
 }
 //--------------------------------------------------------------------------------------------
 
@@ -57,26 +66,6 @@ function showAcquisto() {
     hideAllPages();
     setupPageAscquisto();
 }
-
-
-//funzione per salvare la lista di idAsta visitate
-export function saveVisited(idAsta) {
-    const key = 'asteLastVisited';
-    var oneWeek = 7 * 24 * 60 * 60;
-    // Leggi il cookie e parsa l'array, oppure crea uno vuoto
-    const stored = getCookie(key);
-    const visits = stored ? JSON.parse(stored) : [];
-
-    // Se non c'è già, aggiungi l'id e riscrivi il cookie
-    if (!visits.includes(idAsta)) {
-        visits.push(idAsta);
-        document.cookie = 
-            key + '=' + encodeURIComponent(visits) +
-            '; max-age=' + oneWeek +
-            '; path=/';
-    }
-}
-
 
 export function hideAllPages() {
     document.getElementById('vendoPage').hidden = true;
